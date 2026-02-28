b9df424a8db5bde704f8e98c64c82f94e92157ab
adithya <adithyasivaraman5@gmail.com>
2026-02-28 14:26:43 +0530

    [Adithya] Added : UC4 : Extended Unit Support

---
+++ src/QuantityLength.java
@@ -0,0 +1,28 @@
+public class QuantityLength {
+
+    private final double value;
+    private final LengthUnit unit;
+
+    public QuantityLength(double value, LengthUnit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    private double toFeet() {
+        return unit.toFeet(value);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        QuantityLength other = (QuantityLength) obj;
+
+        return Double.compare(this.toFeet(), other.toFeet()) == 0;
+    }
+}
--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,103 +1,14 @@
-/**
- * QuantityMeasurementApp – UC3: Generic Quantity Class for DRY Principle
- *
- * Refactors Feet and Inches classes into a single QuantityLength class
- * using LengthUnit enum.
- *
- * Supports:
- * - Feet equality
- * - Inches equality
- * - Feet ↔ Inches equality
- */
-
 public class QuantityMeasurementApp {
-
-    /*
-     * LengthUnit Enum
-     * Defines conversion factors relative to Feet
-     */
-    public enum LengthUnit {
-
-        FEET(1.0),
-        INCHES(1.0 / 12.0);
-
-        private final double conversionFactor;
-
-        LengthUnit(double conversionFactor) {
-            this.conversionFactor = conversionFactor;
-        }
-
-        public double getConversionFactor() {
-            return conversionFactor;
-        }
-    }
-
-
-
-    /*
-     * Generic QuantityLength Class
-     */
-    public static class QuantityLength {
-
-        private final double value;
-        private final LengthUnit unit;
-
-        public QuantityLength(double value, LengthUnit unit) {
-
-            if (unit == null)
-                throw new IllegalArgumentException("Unit cannot be null");
-
-            this.value = value;
-            this.unit = unit;
-        }
-
-
-        private double toFeet() {
-            return value * unit.getConversionFactor();
-        }
-
-
-        @Override
-        public boolean equals(Object obj) {
-
-            // Same reference
-            if (this == obj)
-                return true;
-
-            // Null or type check
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-
-            QuantityLength other = (QuantityLength) obj;
-
-            // Convert to common unit (Feet)
-            return Double.compare(this.toFeet(), other.toFeet()) == 0;
-        }
-    }
-
-
 
     public static void main(String[] args) {
 
         QuantityLength q1 =
-                new QuantityLength(1.0, LengthUnit.FEET);
+                new QuantityLength(1.0, LengthUnit.YARDS);
 
         QuantityLength q2 =
-                new QuantityLength(12.0, LengthUnit.INCHES);
+                new QuantityLength(3.0, LengthUnit.FEET);
 
-        boolean result1 = q1.equals(q2);
-
-        System.out.println("Equal (" + result1 + ")");
-
-
-        QuantityLength q3 =
-                new QuantityLength(1.0, LengthUnit.INCHES);
-
-        QuantityLength q4 =
-                new QuantityLength(1.0, LengthUnit.INCHES);
-
-        boolean result2 = q3.equals(q4);
-
-        System.out.println("Equal (" + result2 + ")");
+        System.out.println("Equal (" +
+                q1.equals(q2) + ")");
     }
 }
